15e85325a3f0674efa957a44be4a4deaef81c7f1
logongas <lorenzo.profesor@gmail.com>
2025-07-29 16:08:17 +0200

    Volver a poner el tamaño del GC para compilar

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "Matando procesos gradle y node colgados..."
 pkill -f '.*gradle.*' 2>/dev/null
 pkill -f '.*node.*' 2>/dev/null
 
-export NODE_OPTIONS=--max-old-space-size=9216
-export GRADLE_OPTS="-Xmx9g"
+export NODE_OPTIONS=--max-old-space-size=8192
+export GRADLE_OPTS="-Xmx8g"
 
 ./gradlew --no-daemon clean
 ./gradlew --no-daemon build --stacktrace
