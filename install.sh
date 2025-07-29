84c1c1ce58cc89ed7279d2bec8fe7ce57e8a298f
logongas <lorenzo.profesor@gmail.com>
2025-07-29 18:47:08 +0200

    Quitar el tamaño del GC

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-clear
-echo "Matando procesos gradle y node colgados..."
-pkill -f '.*gradle.*' 2>/dev/null
-pkill -f '.*node.*' 2>/dev/null
+#clear
+#echo "Matando procesos gradle y node colgados..."
+#pkill -f '.*gradle.*' 2>/dev/null
+#pkill -f '.*node.*' 2>/dev/null
 
-export NODE_OPTIONS=--max-old-space-size=8192
-export GRADLE_OPTS="-Xmx8g"
+#export NODE_OPTIONS=--max-old-space-size=8192
+#export GRADLE_OPTS="-Xmx8g"
 
 ./gradlew --no-daemon clean
 ./gradlew --no-daemon build --stacktrace
