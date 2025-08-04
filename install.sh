d1780820637aac1fdfe820a60d4e73d2ff7dc934
logongas <lorenzo.profesor@gmail.com>
2025-08-04 23:44:42 +0200

    Mostrar la fecha al acabar de instalar

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ export NODE_OPTIONS=--max-old-space-size=6096
 export GRADLE_OPTS="-Xmx2g"
 
 ./gradlew --no-daemon clean
-./gradlew --no-daemon build --stacktrace -x test  -x :axelor-common:test  -x :axelor-core:test  -x :axelor-gradle:test -x :axelor-test:test -x :axelor-tomcat:test -x :axelor-tools:test -x :axelor-web:test
+#./gradlew --no-daemon build --stacktrace -x test  -x :axelor-common:test  -x :axelor-core:test  -x :axelor-gradle:test -x :axelor-test:test -x :axelor-tomcat:test -x :axelor-tools:test -x :axelor-web:test
+./gradlew --no-daemon build --stacktrace
 ./gradlew --no-daemon publishToMavenLocal
 
-#./gradlew --no-daemon build --stacktrace -x test
-#./gradlew --no-daemon publishToMavenLocal
+date
